5c8a92ed9027f7b3aad42a6967ea7b3fd015f4d0
Victor <victorsouzalima2064@gmail.com>
2025-05-28 18:20:24 -0300

    data treatment complete for random forest

--- Utils/pre_processing.py
+++ Utils/pre_processing.py
@@ -2,15 +2,20 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
 
-def treat_data(data, columns_to_remove = None, essential_columns = None, label_encoding_columns = None):
+def treat_data(
+        data, 
+        columns_to_remove = None, 
+        essential_columns = None, 
+        label_encoding_columns = None,
+    ):
 
-    if columns_to_remove != None:
+    if columns_to_remove:
         data = data.drop(columns_to_remove, axis=1, errors='ignore')
     
-    if essential_columns != None:
+    if essential_columns:
         data = data.dropna(subset = essential_columns)
 
-    if label_encoding_columns != None:
+    if label_encoding_columns:
         for col in label_encoding_columns:
             le = LabelEncoder()
             data[col] = le.fit_transform(data[col].astype(str))
--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from Utils.data_set_reader import csv_reader
 from Utils.pre_processing import treat_data
+import pandas as pd 
+from sklearn.preprocessing import LabelEncoder
+
 
 file_name = "titanic_data_set/train.csv"
 
-data = csv_reader(file_name)
+data_train = csv_reader(file_name)
+data_test = csv_reader(file_name)
 
-columns_to_remove = ["PassengerId", "Ticket", "Cabin", "Embarked", "Name"]
-essential_columns = ["Survived", "Pclass", "Sex", "Age", "SibSp", "Parch", "Fare"]
+columns_to_remove = ["PassengerId", "Ticket", "Cabin", "Embarked", "Name", "Fare"]
+essential_columns = ["Survived", "Pclass", "Sex", "Age", "SibSp", "Parch"]
 label_encoding_columns = ["Sex"]
 
-data = treat_data(data, columns_to_remove, essential_columns, label_encoding_columns)
+data_train = treat_data(data_train, columns_to_remove, essential_columns, label_encoding_columns)
+data_test = treat_data(data_test, columns_to_remove, essential_columns, label_encoding_columns)
 
-print(data)
+X_train = data_train.drop('Survived', axis=1)
+y_train = data_train['Survived']
+
+X_test = data_test
+
+print(data_train)
