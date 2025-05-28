611c923a3dfad012fe92bc3a6ab8ae0f6769ae3d
Victor <victorsouzalima2064@gmail.com>
2025-05-27 23:25:35 -0300

    csv_reader function implemented

---
+++ Utils/data_set_reader.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def csv_reader(file_name):
+    data = pd.read_csv(file_name)
+
+    return data
---
+++ main.py
@@ -0,0 +1,7 @@
+from Utils.data_set_reader import csv_reader
+
+file_name = "titanic_data_set/test.csv"
+
+data = csv_reader(file_name)
+
+print(data)
