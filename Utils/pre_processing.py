4ee184cb5523cea8cfddd414384da1a015ece5d0
Victor <victorsouzalima2064@gmail.com>
2025-05-30 13:40:56 -0300

    new pre_processing methods implemented

--- Utils/pre_processing.py
+++ Utils/pre_processing.py
@@ -1,12 +1,13 @@
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
-
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 
 def treat_data(
         data, 
         columns_to_remove = None, 
         essential_columns = None, 
         label_encoding_columns = None,
+        one_hot_columns = None,
+        columns_to_scale = None
     ):
 
     if columns_to_remove:
@@ -19,5 +20,12 @@ def treat_data(
         for col in label_encoding_columns:
             le = LabelEncoder()
             data[col] = le.fit_transform(data[col].astype(str))
+
+    if one_hot_columns:
+        data = pd.get_dummies(data, columns=one_hot_columns, drop_first=False, dtype=int)
+
+    if columns_to_scale:
+        scaler = StandardScaler()
+        data[columns_to_scale] = scaler.fit_transform(data[columns_to_scale])
 
     return data
