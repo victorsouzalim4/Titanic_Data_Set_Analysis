a8962291556d1471d75f90d11e4dd0de095a9418
Victor <victorsouzalima2064@gmail.com>
2025-05-30 22:02:35 -0300

    test implemented

--- Algorithms/Neural_network/Neural_network.py
+++ Algorithms/Neural_network/Neural_network.py
@@ -2,7 +2,7 @@ from Neural_Networks_Sub.Back_Propagation.back_propagation import backPropagatio
 from Neural_Networks_Sub.Neural_Network.test_neural_network import testNeuralNetwork
 from Utils.data_set_reader import csv_reader
 from Utils.pre_processing import treat_data
-from Utils.metrics_visual import plot_roc_curve, plot_survival_distribution
+from Utils.metrics_visual import plot_roc_curve, plot_survival_distribution, save_confusion_matrix, save_classification_report_image
 from imblearn.over_sampling import SMOTE
 import pandas as pd
 
@@ -24,7 +24,47 @@ def neural_network():
         columns_to_scale = columns_to_normalize
     )
 
+    X_test = data_test.dropna().astype(float)
+
     model = train_model()
+
+
+    left_indexes = X_test.index
+
+    y_true = true_labels['Survived'].replace(0, -1)
+    y_test = testNeuralNetwork(layers = model, test_inputs = X_test, test_expected_outputs = y_true, activation="tanh")
+
+    y_true_clean = y_true.loc[left_indexes]
+
+    # y_proba_test = model.predict_proba(X_test)[:, 1]
+
+    # plot_roc_curve(
+    #     y_true=y_true_clean,
+    #     y_proba=y_proba_test,
+    #     title='Curva ROC - Random Forest (Test)',
+    #     save_path='Analysis/Random_forest/roc_curve_rf_test.png',
+    #     show=True
+    # )
+
+    y_test_metrics = [0 if y == -1 else 1 for y in y_test]
+    y_true_metrics = y_true_clean.replace(-1, 0)
+
+    save_confusion_matrix(
+        y_true=y_true_metrics,
+        y_pred=y_test_metrics,
+        class_labels=["Died", "Survived"],
+        save_path="Analysis/Random_forest/conf_matrix_rf_test.png",
+        show=True,
+        title="Confusion Matrix - Test Set"
+    )
+
+    save_classification_report_image(
+        y_true=y_true_metrics,
+        y_pred=y_test_metrics,
+        class_labels=["Died", "Survived"],
+        save_path="Analysis/Random_forest/classification_report_rf_test.png",
+        title="Classification Report - Random Forest"
+    )
 
 def train_model():
 
@@ -43,8 +83,10 @@ def train_model():
         columns_to_scale = columns_to_normalize
     )
 
-    X_train = data_train.drop('Survived', axis=1)
-    y_train = data_train['Survived']
+    X_train = data_train.drop('Survived', axis=1).astype(float)
+    y_train = data_train['Survived'].replace(0, -1)
+
+
 
     plot_survival_distribution(
         train_labels=y_train,
@@ -67,11 +109,11 @@ def train_model():
     print(y_resampled)
 
     model = backPropagation(
+        4, 
         3, 
-        3, 
-        X_resampled.to_numpy().tolist(), 
+        X_resampled.astype(float).to_numpy().tolist(), 
         y_resampled.to_numpy().tolist(), 
-        2000, 
+        1000, 
         0.01, 
         "Titanic", 
         "tanh", 
